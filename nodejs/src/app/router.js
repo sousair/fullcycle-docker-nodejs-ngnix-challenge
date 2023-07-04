9c475b4355ee14e2714d93e00ac9dfbf6ec7713d
Iago Sousa <sousair2001@gmail.com>
2023-07-03 21:48:37 -0300

    feat: add router and json middleware

---
+++ nodejs/src/app/router.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ ok: true })
+});
+
+module.exports = {
+  router,
+};
--- nodejs/src/app/server.js
+++ nodejs/src/app/server.js
@@ -4,6 +4,9 @@ const { APP_PORT } = require('./config');
 
 const server = express();
 
+server.use(express.json());
+server.use(router); 
+
 server.listen(APP_PORT, () => {
   console.log(`Server listen on port ${APP_PORT}`)
 })
